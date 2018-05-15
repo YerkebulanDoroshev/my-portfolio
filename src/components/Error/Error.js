a60fd36c55424222962a9fe898da50d450bb6be6
Yerke <erke01vip@gmail.com>
2018-05-16 00:45:51 +0600

    added new components

---
+++ src/components/Error/Error.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div>
+      <p>Error path is not exist</p>
+    </div>
+  )
+}
+
+export default Error;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const About = () => {
+const Home = () => {
   return (
     <div>
-      <p>About</p>
+      <p>Home</p>
     </div>
   )
 }
 
-export default About;
+export default Home;
