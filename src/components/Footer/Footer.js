dab8bf6ef3b0a4d52d5b4204e619aa35c3c9352e
Yerke <erke01vip@gmail.com>
2018-05-17 05:31:54 +0600

    Footer setting

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,19 +8,18 @@ const Footer = () => {
       <footer>
         <div className="d-flex bd-highlight justify-content-around flex-wrap">
           <div className="p-2 flex-fill bd-highlight">
-            <h2>Doroshev Yerkebulan</h2>
-            <h6>Web-developer, UI-designer, ReactJS</h6>
+            <p className="owner-name">Doroshev Yerkebulan</p>
+            <p className="owner-description">Web-developer, UI-designer, ReactJS</p>
           </div>
 
-          <div className="p-2 flex-fill bd-highlight">
-            <h5>Links:</h5>
+          <div className="p-2 flex-fill bd-highlight links">
             <a href="https://www.instagram.com/y.e.r.k.e/"><FontAwesome.FaInstagram size={60} /></a>
             <a href="https://github.com/YerkebulanDoroshev"><FontAwesome.FaGithub size={60} /></a>
             <a href="https://www.linkedin.com/in/yerkebulan"><FontAwesome.FaLinkedin size={60} /></a>
           </div>
         </div>
-        <h6>Almaty, Kazakhstan</h6>
-        <p>2016 - {(new Date().getFullYear())}</p>
+        <p className="city-name">Almaty, Kazakhstan</p>
+        <p className="years-of-living">2016 - {(new Date().getFullYear())}</p>
       </footer>
     </div>
   )
