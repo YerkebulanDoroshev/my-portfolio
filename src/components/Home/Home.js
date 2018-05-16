81e4df8f3594606ecd89716c2610ff9e3e9b0ea1
Yerke <erke01vip@gmail.com>
2018-05-16 16:52:52 +0600

    added in Home main sector

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,54 @@
 import React from 'react'
+import './Home.css'
+import logo from './../../static/index.png'
 
 const Home = () => {
   return (
-    <div class="jumbotron jumbotron-fluid">
-      <div class="container">
-        <h1 class="display-3">Fluid jumbotron</h1>
-        <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-      </div>
+    <div>
+      <header>
+        <div className="jumbotron jumbotron-fluid">
+          <div className="container">
+            <h1 className="display-3">Welcome</h1>
+            <p className="lead">Portfolio of design, web, mobile development</p>
+          </div>
+        </div>
+      </header>
+      
+      <main>
+        <div className="container d-flex justify-content-between">
+          <div className="card" >
+            <img className="card-img-top" src={logo} alt="Card cap" />
+            <div className="card-block">
+              <h4 className="card-title">Card title</h4>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a className="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+          <div className="card" >
+            <img className="card-img-top" src={logo} alt="Card cap" />
+            <div className="card-block">
+              <h4 className="card-title">Card title</h4>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a className="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+          <div className="card" >
+            <img className="card-img-top" src={logo} alt="Card cap" />
+            <div className="card-block">
+              <h4 className="card-title">Card title</h4>
+              <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a className="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <footer>
+        <div>
+          This is footer
+        </div>
+      </footer>
+      
     </div>
   )
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-import './Navigation.js'
+import './Navigation.css'
 
 const Navigation = () => {
   return (
