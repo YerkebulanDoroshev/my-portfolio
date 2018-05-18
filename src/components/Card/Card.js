4c7cf5481fb7fa482a9e264c3db2497078e16445
Yerke <erke01vip@gmail.com>
2018-05-18 17:22:57 +0600

    Remaked card and added props in main

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,21 +1,14 @@
 import React from 'react'
-import data from './../../static/portfolio.json'
-import photo1 from './../../static/covers/1.png'
+
 import './Card.css'
 
 const Card = (props) => {
-  var photos = [];
-  for (var i = 0; i < 3; i++) {
-    photos.push(
-    
-    );
-  } 
   return (
     <div className="card" >
-      <img className="card-img-top" src={photo1} alt="Card cap" />
+      <img className="card-img-top" src={props.photo} alt="Card cap" />
       <div className="d-flex flex-column card-block">
-        <h4 className="card-title">{data[props.var].title}</h4>
-        <p className="card-text">{data[props.var].shortDescription}</p>
+        <h4 className="card-title">{props.title}</h4>
+        <p className="card-text">{props.shortDescription}</p>
         <a className="btn">Подробнее</a>
       </div>
     
--- src/components/Home/Main/Main.js
+++ src/components/Home/Main/Main.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react'
 import Card from './../../Card/Card'
 import './Main.css'
+import logo1 from './../../../static/covers/1.png'
+import logo2 from './../../../static/covers/2.png'
+import logo3 from './../../../static/covers/3.png'
+import { NavLink } from 'react-router-dom'
 
  class Main extends Component {
   render() {
-    var rows = [];
-    for (var i = 0; i < 3; i++) {
-      rows.push(
-        <Card var={i} />
-      );
-    } 
     return (
       <div className="Main">
         <main>
           <div className="container d-flex justify-content-around flex-wrap">
-            {rows}
+            <Card 
+              photo={logo1} 
+              title="Turanter"
+              shortDescription="Tурецкая туристическая компания"
+            />
+              
+            <Card 
+              photo={logo2}
+              title="Lightning Design"
+              shortDescription="Компания по веб-разработке и дизайна"
+            />
+            <Card 
+              photo={logo3} 
+              title="Mezunion"
+              shortDescription="Студенческая благотворительная организация"
+            />            
           </div>
+          <div className="container d-flex justify-content-around flex-wrap">
+            <NavLink className="btn view-all" to="/portfolio">Все работы</NavLink>  
+          </div>
+          
         </main>
       </div>
     )
