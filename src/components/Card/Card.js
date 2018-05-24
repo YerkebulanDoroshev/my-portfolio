987350fc589fc2f096ba06ea489510a183ad935a
Yerke <erke01vip@gmail.com>
2018-05-24 21:16:56 +0600

    Added animation transition to card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,16 +4,17 @@ import './Card.css'
 
 const Card = (props) => {
   return (
-    <div className="card" >
-      {/* <img className="card-img-top" src={props.photo} alt="Card cap" /> */}
-     
-      <a href="" className="card-block" style={{ backgroundImage: `url(${props.photo})`}}>
-        <div className="d-flex flex-column ">
-          <h4 className="card-title">{props.title}</h4>
-          <p className="card-text">{props.shortDescription}</p>
-        </div>
-      </a> 
-    </div>
+    <a href="" className="card" >
+      <div className="card-block" style={{ backgroundImage: `url(${props.photo})` }}>
+        
+      </div>
+      <div className="card-title">
+        <h4 >{props.title}</h4>
+        <p>{props.shortDescription}</p>
+      </div>
+       
+    </a>
+
   )
 }
 export default Card;
