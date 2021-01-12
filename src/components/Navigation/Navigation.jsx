fe83d3d2b33490a31e0b277314ff07cf672d4642
Yerkebulan <160103106@stu.sdu.edu.kz>
2021-01-13 00:53:12 +0600

    feat: changed main color

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,41 +1,67 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-import './Navigation.css'
+import "./Navigation.css";
 
 const Navigation = () => {
   return (
     <div className="Navigation">
-        <ul className="nav justify-content-center">
-          <li className="nav-item">
-            <a className="nav-link home">
-              <NavLink className="nav-link" to="/">Doroshev.kz</NavLink>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link">
-              <NavLink className="nav-link" to="/portfolio">Portfolio</NavLink>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link">
-              <NavLink className="nav-link" to="/about">About</NavLink>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link">
-              <NavLink className="nav-link" to="/contacts">Contacts</NavLink>
-            </a>
-          </li>
-           <li className="nav-item">
-            <a className="nav-link">
-              <NavLink className="nav-link" to="/resume">Resume</NavLink>
-            </a>
-          </li>
-        </ul>
+      <ul className="nav justify-content-center">
+        <li className="nav-item">
+          <a className="nav-link">
+            <NavLink
+              className="nav-link"
+              activeClassName="active-link"
+              exact
+              to="/"
+            >
+              Yerkeboolean
+            </NavLink>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link">
+            <NavLink
+              className="nav-link"
+              activeClassName="active-link"
+              to="/portfolio"
+            >
+              Portfolio
+            </NavLink>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link">
+            <NavLink
+              className="nav-link"
+              activeClassName="active-link"
+              to="/about"
+            >
+              About
+            </NavLink>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link">
+            <NavLink
+              className="nav-link"
+              activeClassName="active-link"
+              to="/contacts"
+            >
+              Contacts
+            </NavLink>
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link">
+            <NavLink className="nav-link" to="/resume">
+              Resume
+            </NavLink>
+          </a>
+        </li>
+      </ul>
     </div>
-    
-  )
-}
+  );
+};
 
 export default Navigation;
