d430d663c05c5502d995369d8d3a441e6dd51cb3
Yerkebulan <160103106@stu.sdu.edu.kz>
2021-01-14 05:25:00 +0600

    feat: top navigation redesign and footer layout fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,48 +1,54 @@
-import React from 'react'
-import * as FontAwesome from 'react-icons/lib/fa'
-import './Footer.css'
+import React from "react";
+import * as FontAwesome from "react-icons/lib/fa";
+import "./Footer.css";
 
 const Footer = () => {
   return (
     <div className="Footer">
       <footer>
-        <div className="d-flex bd-highlight justify-content-around flex-wrap">
+        <div className="d-flex bd-highlight justify-content-between flex-wrap">
           <div className="p-2 flex-fill bd-highlight">
             <p className="owner-name">Doroshev Yerkebulan</p>
-            <p className="owner-description">Web-developer, UI-designer, ReactJS</p>
+            <p className="owner-description">
+              Web-developer, UI-designer, ReactJS
+            </p>
           </div>
 
           <div className="p-2 flex-fill bd-highlight links">
-            <a 
-              href="https://www.instagram.com/yerkeboolean/" 
-              target="_blank" 
-              rel="noopener noreferrer">
-                <FontAwesome.FaInstagram size={60} />
+            <a
+              href="https://www.instagram.com/yerkeboolean/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesome.FaInstagram size={60} />
             </a>
-            <a 
-              href="https://github.com/yerkeboolean" 
-              target="_blank" 
-              rel="noopener noreferrer">
-                <FontAwesome.FaGithub size={60} />
+            <a
+              href="https://github.com/yerkeboolean"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesome.FaGithub size={60} />
             </a>
-            <a 
-              href="https://www.linkedin.com/in/yerkebulan" 
-              target="_blank" 
-              rel="noopener noreferrer">
-                <FontAwesome.FaLinkedin size={60} />
+            <a
+              href="https://www.linkedin.com/in/yerkebulan"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesome.FaLinkedin size={60} />
             </a>
-            <a 
-              href="https://t.me/Doroshev" 
-              target="_blank" 
-              rel="noopener noreferrer">
-                <FontAwesome.FaPaperPlane size={60} />
+            <a
+              href="https://t.me/Doroshev"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesome.FaPaperPlane size={60} />
             </a>
           </div>
         </div>
         <p className="city-name">Almaty, Kazakhstan</p>
-        <p className="years-of-living">{(new Date().getFullYear())}</p>
+        <p className="years-of-living">{new Date().getFullYear()}</p>
       </footer>
     </div>
-  )
-}
+  );
+};
 export default Footer;
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,66 +1,55 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-import "./Navigation.css";
+import classes from "./Navigation.module.css";
 
 const Navigation = () => {
   return (
-    <div className="Navigation">
-      <ul className="nav justify-content-center">
-        <li className="nav-item">
-          <a className="nav-link">
-            <NavLink
-              className="nav-link"
-              activeClassName="active-link"
-              exact
-              to="/"
-            >
-              Yerkeboolean
-            </NavLink>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link">
-            <NavLink
-              className="nav-link"
-              activeClassName="active-link"
-              to="/portfolio"
-            >
-              Portfolio
-            </NavLink>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link">
-            <NavLink
-              className="nav-link"
-              activeClassName="active-link"
-              to="/about"
-            >
-              About
-            </NavLink>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link">
-            <NavLink
-              className="nav-link"
-              activeClassName="active-link"
-              to="/contacts"
-            >
-              Contacts
-            </NavLink>
-          </a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link">
-            <NavLink className="nav-link" to="/resume">
-              Resume
-            </NavLink>
-          </a>
-        </li>
-      </ul>
-    </div>
+    <nav className={classes.Navigation}>
+      <div className={classes.logo}>
+        <NavLink
+          className={classes.navLink}
+          activeClassName={classes.activeLink}
+          exact
+          to="/"
+        >
+          Yerkeboolean
+        </NavLink>
+      </div>
+
+      <div className={classes.links}>
+        <NavLink
+          className={classes.navLink}
+          activeClassName={classes.activeLink}
+          to="/portfolio"
+        >
+          Portfolio
+        </NavLink>
+
+        <NavLink
+          className={classes.navLink}
+          activeClassName={classes.activeLink}
+          to="/about"
+        >
+          About
+        </NavLink>
+
+        <NavLink
+          className={classes.navLink}
+          activeClassName={classes.activeLink}
+          to="/contacts"
+        >
+          Contacts
+        </NavLink>
+        <NavLink
+          className={classes.navLink}
+          activeClassName={classes.activeLink}
+          to="/resume"
+        >
+          Resume
+        </NavLink>
+      </div>
+    </nav>
   );
 };
 
