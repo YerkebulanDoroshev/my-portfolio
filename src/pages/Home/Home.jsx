2d7983062e5257149151f7fb835d34b19535a07a
Yerkebulan <160103106@stu.sdu.edu.kz>
2021-01-14 22:15:48 +0600

    feat: add particles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,19 +1,100 @@
-import React from 'react'
-import Main from './Main/Main'
-import Header from './Header/Header'
+import React from "react";
+import Main from "./Main/Main";
+import Header from "./Header/Header";
+import Particles from "react-tsparticles";
 
-
-import './Home.css'
+import "./Home.css";
 
 const Home = (props) => {
-    
-    return (
-      <div className="Home">
-        <Header />
+  return (
+    <div className="Home">
+      <Particles
+        style={{ position: "absolute" }}
+        id="tsparticles"
+        options={{
+          background: {
+            color: {
+              value: "#ffffff",
+            },
+          },
+          fpsLimit: 60,
+          interactivity: {
+            detectsOn: "canvas",
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "repulse",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 400,
+                duration: 2,
+                opacity: 0.8,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#0a46bf",
+            },
+            links: {
+              color: "#0a46bf",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+      <Header />
 
-        <Main />
-      </div>
-    )
-}
+      <Main />
+    </div>
+  );
+};
 
 export default Home;
--- src/pages/Home/Main/Main.jsx
+++ src/pages/Home/Main/Main.jsx
@@ -1,43 +1,47 @@
-import React, { Component } from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
-import {Card} from '../../../components'
-import './Main.css'
+import classes from "./Main.module.css";
+import { Card } from "../../../components";
 
-import logo1 from './../../../static/covers/1.png'
-import logo2 from './../../../static/covers/2.png'
-import logo3 from './../../../static/covers/3.png'
+import logo1 from "./../../../static/covers/1.png";
+import logo2 from "./../../../static/covers/2.png";
+import logo3 from "./../../../static/covers/3.png";
 
- class Main extends Component {
+class Main extends Component {
   render() {
     return (
-      <div className="Main">
+      <div className={classes.Main}>
         <main>
-          <div className="container d-flex justify-content-around flex-wrap">
-            <Card 
-              photo={logo1} 
+          <div className={`container d-flex justify-content-around flex-wrap`}>
+            <Card
+              photo={logo1}
               title="Turanter"
               shortDescription="Tурецкая туристическая компания"
             />
-              
-            <Card 
+
+            <Card
               photo={logo2}
               title="Lightning Design"
               shortDescription="Компания по веб-разработке и дизайна"
             />
-            <Card 
-              photo={logo3} 
+            <Card
+              photo={logo3}
               title="Mezunion"
               shortDescription="Студенческая благотворительная организация"
-            />            
+            />
           </div>
           <div className="container d-flex justify-content-around flex-wrap">
-            <NavLink className="btn view-all" to="/portfolio">Все работы</NavLink>  
+            <NavLink
+              className={`btn view-all ${classes.viewAll}`}
+              to="/portfolio"
+            >
+              Все работы
+            </NavLink>
           </div>
-          
         </main>
       </div>
-    )
+    );
   }
 }
 export default Main;
